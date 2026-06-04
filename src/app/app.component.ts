efac9b65ebd623011bcaefa981abec7ce42e6480
bmichalski <b.michalski@avsystem.com>
2026-06-04 15:01:56 +0200

    add flags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,6 +65,7 @@ export class AppComponent implements OnInit {
   countryToGuess = signal<Country>(randomCountry());
   guesses = signal<Guess[]>([]);
   guessed = signal(false);
+  showFlag = signal(false);
   gameOver = computed(() => this.guessed() || this.guesses().length === 6);
   errorMessage = signal('');
   countryToGuessName = computed(() =>
@@ -135,6 +136,7 @@ export class AppComponent implements OnInit {
     this.countryToGuess.set(this.challengeSequence[0]);
     this.guesses.set([]);
     this.guessed.set(false);
+    this.showFlag.set(false);
     this.fc.enable();
     this.fc.reset();
     this.errorMessage.set('');
@@ -184,6 +186,7 @@ export class AppComponent implements OnInit {
     this.countryToGuess.set(this.challengeSequence[nextIdx]);
     this.guesses.set([]);
     this.guessed.set(false);
+    this.showFlag.set(false);
     this.fc.enable();
     this.fc.reset();
     this.errorMessage.set('');
@@ -201,6 +204,7 @@ export class AppComponent implements OnInit {
     this.countryToGuess.set(randomCountry());
     this.guesses.set([]);
     this.guessed.set(false);
+    this.showFlag.set(false);
     this.fc.enable();
     this.fc.reset();
     this.errorMessage.set('');
@@ -256,6 +260,7 @@ export class AppComponent implements OnInit {
     this.countryToGuess.set(randomCountry());
     this.guesses.set([]);
     this.guessed.set(false);
+    this.showFlag.set(false);
     this.errorMessage.set('');
     this.fc.enable();
     this.fc.reset();
